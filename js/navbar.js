1252604f4f03203f88f3b05eea3f1c6cc454b282
RobinH <Robin.Hawiz.6670@student.uu.se>
2024-12-30 21:20:16 +0100

    The hamburger menu now has improved accessibility by using a customized aria-label when opening and closing

--- js/navbar.js
+++ js/navbar.js
@@ -18,6 +18,7 @@ let animationComplete = true;
 // Added the attributes here because I forgot to add it inside the HTML from the beginning. Otherwise I'd have to go through every single html page to add these.
 burgerking.setAttribute("aria-expanded", false);
 burgerking.setAttribute("aria-controls", "navbar");
+burgerking.setAttribute("aria-label", "Meny");
 nav.setAttribute("id", "navbar");
 
 // Event listeners
@@ -41,6 +42,8 @@ function toggleNavbar() {
   animationComplete = false;
   if (!nav.classList.contains("opened")) {
     menu.innerText = "Stäng";
+    // We set the aria-label to "Stäng meny" for people who use screen readers, otherwise the menu will be read as "Stäng", which might not make as much sense.
+    burgerking.setAttribute("aria-label", "Stäng meny");
     // Adds styling (display: block;).
     nav.classList.add("display-block");
     // Starts an animation that show the navbar.
@@ -54,6 +57,7 @@ function toggleNavbar() {
     );
   } else {
     menu.innerText = "Meny";
+    burgerking.setAttribute("aria-label", "Meny");
     // Removing the showNavbar class plays the hide animation, because the nav element has the property (animate: hideNavbar 0.5s;).
     nav.classList.remove("showNavbar");
     nav.addEventListener(
